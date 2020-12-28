254b220fa8d5df8f8a45057a9657d0f7e9b2ebd9
Elis Lulja <elulja@cisco.com>
2020-12-28 11:15:23 +0100

    Create backend package
    Signed-off-by: Elis Lulja <elulja@cisco.com>

---
+++ pkg/backend/interface.go
@@ -0,0 +1,20 @@
+package backend
+
+import "github.com/SunSince90/kube-scraper-backend/pkg/pb"
+
+// Backend is a backend that can be used to store and retrieve
+// chats.
+type Backend interface {
+	// GetChatByID retrieves a chat by the id
+	GetChatByID(int64) (*pb.Chat, error)
+	// GetChatByUsername retrieves a chat by username
+	GetChatByUsername(string) (*pb.Chat, error)
+	// GetAllChatIDs gets all chats
+	GetAllChats() ([]*pb.Chat, error)
+	// StoreChat stores a new chat in the database
+	StoreChat(*pb.Chat) error
+	// DeleteChat deletes a chat from the database
+	DeleteChat(int64) error
+	// Close any client
+	Close()
+}
